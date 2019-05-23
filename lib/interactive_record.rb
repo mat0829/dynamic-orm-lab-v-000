3fd6d218d5f3166f31b4db1c593f7d33140fd269
David Anderson <matandersonlv@hotmail.com>
2019-05-23 16:19:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,6 +47,9 @@ class InteractiveRecord
     values.join(", ")
   end
   
-  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
+    DB[:conn].execute(sql, name)
+  end
 
 end

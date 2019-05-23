8bc55caf49a17fa05673aa274df4faff2fea1408
David Anderson <matandersonlv@hotmail.com>
2019-05-23 16:07:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,45 +7,41 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
-  
+
   def self.column_names
-    DB[:conn].results_as_hash = true
-    sql = "PRAGMA table_info('#{table_name}')"
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |column|
-      column_names << column["name"]
+    table = DB[:conn].execute("PRAGMA table_info('#{self.table_name}')")
+    table.collect{|column| column['name']}.compact
   end
-    column_names.compact
+
+  def table_name_for_insert
+    self.class.table_name
   end
-  
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if{|column| column == "id"}.join(", ")
   end
-  
+
+  def values_for_insert
+    columns = self.class.column_names.delete_if{|column| column == "id"}
+    columns.collect{|column|"'#{send(column)}'"}.join(", ")
+  end
+
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-    self
   end
-  
-  def table_name_for_insert
-    self.class.table_name
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end
-  
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-  
-  def values_for_insert
-    values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
+
+  def self.find_by(hash)
+    column = hash.keys.join(', ')
+    value = hash.values.join(', ')
+    sql = "SELECT * FROM #{self.table_name} WHERE #{column} = '#{value}'"
+    DB[:conn].execute(sql)
   end
 
 end

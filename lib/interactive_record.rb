4187d8c1e5fa1e38224e41641de347be1a855180
David Anderson <matandersonlv@hotmail.com>
2019-05-23 15:38:25 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,7 +41,7 @@ class InteractiveRecord
   def save
     sql = <<-SQL
     "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) 
-    VALUES (#{values_for_insert});"
+    VALUES (#{values_for_insert})"
     SQL
     DB[:conn].execute(sql)
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
